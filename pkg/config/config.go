62993007fc9555a5e16e034a3b9580c1c559e627
Andrii Vakarev <avakarev@gmail.com>
2019-07-26 02:15:21 +0200

    Keep mqtt config details in its own file

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -10,32 +10,6 @@ import (
 	"github.com/spf13/viper"
 )
 
-// MqttConfig represents mqtt connection options
-type MqttConfig struct {
-	Broker    string
-	Port      int
-	BaseTopic string
-	User      string
-	Password  string
-}
-
-// NewMqttConfig returns new value of mqtt config
-func NewMqttConfig() MqttConfig {
-	return MqttConfig{
-		Broker:    viper.GetString("mqtt.broker"),
-		Port:      viper.GetInt("mqtt.port"),
-		BaseTopic: viper.GetString("mqtt.base-topic"),
-		User:      viper.GetString("mqtt.user"),
-		Password:  viper.GetString("mqtt.password"),
-	}
-}
-
-func init() {
-	viper.SetDefault("mqtt.broker", "localhost")
-	viper.SetDefault("mqtt.port", 1883)
-	viper.SetDefault("mqtt.base-topic", "zigbee2mqtt")
-}
-
 // Load reads in config file and ENV variables if set.
 func Load(cfgFile string) {
 	if cfgFile != "" {
