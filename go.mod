d35e38114d6e393e9e71542f4a7416628d8e3ea4
Andrii Vakarev <avakarev@gmail.com>
2019-07-27 23:43:22 +0200

    * Extend `zimt config` command with broker's config

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/radiohive/zimt
 go 1.12
 
 require (
+	github.com/eclipse/paho.mqtt.golang v1.2.0
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/viper v1.3.2
+	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80 // indirect
 )
---
+++ pkg/api/bridge-config.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	"encoding/json"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/radiohive/zimt/pkg/structs"
+)
+
+// BridgeConfig represents message of `#/bridge/config` topic
+type BridgeConfig struct {
+	Version     string `json:"version"`
+	Commit      string `json:"commit"`
+	Coordinator int    `json:"coordinator"`
+	LogLevel    string `json:"log_level"`
+	PermitJoin  bool   `json:"permit_join"`
+}
+
+// Print prints bridge config to standard output
+func (bc BridgeConfig) Print() {
+	structs.Report(&bc, "json")
+}
+
+// GetBridgeConfig subscribes and returns message  from `#/bridge/config` topic
+func GetBridgeConfig(client mqtt.Client) BridgeConfig {
+	topic := topic("bridge/config")
+	msg := getSubscribedOnce(client, topic)
+	config := BridgeConfig{}
+	json.Unmarshal(msg.Payload(), &config)
+	return config
+}
