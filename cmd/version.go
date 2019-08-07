d154802575b195763f493a48a75b43ca1c01fb46
Andrii Vakarev <avakarev@gmail.com>
2019-08-07 22:43:56 +0200

    Use column to separate keys and values when priting version details

--- cmd/version.go
+++ cmd/version.go
@@ -12,13 +12,13 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Prints zimt version and build information",
 	Run: func(cmd *cobra.Command, args []string) {
-		f := "%-10v%s\n"
-		fmt.Printf(f, "GitTag", buildmeta.GitTag)
-		fmt.Printf(f, "GitCommit", buildmeta.GitCommit)
-		fmt.Printf(f, "GitBranch", buildmeta.GitBranch)
-		fmt.Printf(f, "BuildDate", buildmeta.BuildDate)
-		fmt.Printf(f, "Platform", buildmeta.Platform)
-		fmt.Printf(f, "Compiler", buildmeta.Compiler)
+		f := "%-11v%s\n"
+		fmt.Printf(f, "GitTag:", buildmeta.GitTag)
+		fmt.Printf(f, "GitCommit:", buildmeta.GitCommit)
+		fmt.Printf(f, "GitBranch:", buildmeta.GitBranch)
+		fmt.Printf(f, "BuildDate:", buildmeta.BuildDate)
+		fmt.Printf(f, "Platform:", buildmeta.Platform)
+		fmt.Printf(f, "Compiler:", buildmeta.Compiler)
 	},
 }
 
