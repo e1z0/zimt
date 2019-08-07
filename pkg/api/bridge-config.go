cb2bf76cad5917a05d61bdf53a00d32bab0f914a
Andrii Vakarev <avakarev@gmail.com>
2019-08-07 22:55:12 +0200

    Implement `zimt bridge` command

--- pkg/api/bridge-config.go
+++ pkg/api/bridge-config.go
@@ -2,10 +2,8 @@ package api
 
 import (
 	"encoding/json"
-	"io"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
-	"github.com/radiohive/zimt/pkg/structs"
 )
 
 // BridgeConfig represents message of `#/bridge/config` topic
@@ -15,11 +13,6 @@ type BridgeConfig struct {
 	Coordinator int    `json:"coordinator"`
 	LogLevel    string `json:"log_level"`
 	PermitJoin  bool   `json:"permit_join"`
-}
-
-// Print prints bridge config to standard output
-func (bc BridgeConfig) Print(writer io.Writer) {
-	structs.Print(&bc, "json", writer)
 }
 
 // GetBridgeConfig subscribes and returns message  from `#/bridge/config` topic
