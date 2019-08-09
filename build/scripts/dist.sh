722c725d6f5c95f0d7f179100bf4123d0f6039f5
Andrii Vakarev <avakarev@gmail.com>
2019-08-09 22:38:50 +0200

    Cleanup dist script

--- build/scripts/dist.sh
+++ build/scripts/dist.sh
@@ -1,37 +1,23 @@
 #!/bin/bash
 set -e
 
-platform=$1
+source $(realpath $(dirname $0)/build.env)
 
-if [[ -z $platform ]]; then
-    echo "USAGE: PLATFORM" > /dev/stderr
-    exit 1
-fi
-
-platform_parts=(${platform//\// })
-os=${platform_parts[0]}
-arch=${platform_parts[1]}
-bin="zimt_${os}_${arch}"
-out="dist/${bin}"
-
-module=$(awk '/module/{print $2}' go.mod)
-buildmeta="${module}/pkg/buildmeta"
-
-echo "${out}:"
+echo "${BUILD_OUT}:"
 echo -ne "  Buidling..."
-CGO_ENABLED=0 GOOS=${os} GOARCH=${arch} go build -o ${out} -ldflags "\
-    -X '${buildmeta}.GitTag=$(git describe --abbrev=0)' \
-    -X '${buildmeta}.GitCommit=$(git rev-parse --verify --short HEAD)' \
-    -X '${buildmeta}.GitBranch=$(git symbolic-ref --short -q HEAD)' \
-    -X '${buildmeta}.BuildDate=$(date -u +"%Y-%m-%dT%H:%M:%SZ")' \
-    -X '${buildmeta}.Platform=${platform}' \
+CGO_ENABLED=0 GOOS=${BUILD_OS} GOARCH=${BUILD_ARCH} go build -o ${BUILD_OUT} -ldflags "\
+    -X '${GO_BUILDMETA}.GitTag=${GIT_TAG}' \
+    -X '${GO_BUILDMETA}.GitCommit=${GIT_COMMIT}' \
+    -X '${GO_BUILDMETA}.GitBranch=${GIT_BRANCH}' \
+    -X '${GO_BUILDMETA}.BuildDate=${BUILD_DATE}' \
+    -X '${GO_BUILDMETA}.Platform=${BUILD_PLATFORM}' \
     -extldflags '-static'"
 echo -ne " ✔\n"
 
 echo -ne "  Compressing..."
-cd dist && tar -cvzf ${bin}.tar.gz ${bin} > /dev/null 2>&1 && rm ${bin} && cd ..
+cd dist && tar -cvzf ${BUILD_BIN}.tar.gz ${BUILD_BIN} > /dev/null 2>&1 && cd ..
 echo -ne " ✔\n"
 
 echo -ne "  Calculating sha256..."
-echo $(gsha256sum "${out}.tar.gz") | cut -d " " -f1 > "${out}tar.gz.sha256"
+echo $(gsha256sum "${BUILD_OUT}.tar.gz") | cut -d " " -f1 > "${BUILD_OUT}tar.gz.sha256"
 echo -ne " ✔\n"
